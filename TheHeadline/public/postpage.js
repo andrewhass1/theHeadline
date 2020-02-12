d2a50d8e33793dbebde27f9d1b18cd7cea79085d
Robinson24 <andrew.hass@hotmail.com>
2020-02-12 12:04:58 -0500

    paypal

--- TheHeadline/public/postpage.js
+++ TheHeadline/public/postpage.js
@@ -2,25 +2,7 @@ function publish() {
     var db = firebase.firestore();
     var theTitle = document.getElementById('title').value;
     var theContent = document.getElementById('content').value;
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-          db.collection("posts").add({
-              title: theTitle,
-              content: theContent,
-              user: user.email,
-              time: firebase.firestore.FieldValue.serverTimestamp(),
-              status: "NOTPOSTED"
-          })
-          .then(function(docRef) {
-              console.log("Document written with ID: ", docRef.id);
-          })
-          .catch(function(error) {
-              console.error("Error adding document: ", error);
-          });
-      } else {
-        // No user is signed in.
-      }
-    });
-
-
+    localStorage.setItem("title", theTitle);
+    localStorage.setItem("content", theContent);
+    location.href = 'PayPalPage.html';
 }
