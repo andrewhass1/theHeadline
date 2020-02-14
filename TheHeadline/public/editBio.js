461a86160e310427450497f1e66a7fbee585c309
Robinson24 <andrew.hass@hotmail.com>
2020-02-14 17:20:51 -0500

    new

--- TheHeadline/public/editBio.js
+++ TheHeadline/public/editBio.js
@@ -6,19 +6,18 @@ var db = firebase.firestore();
 function update(){
   var userRef = db.collection("users");
   var bio2 = document.getElementById('bio').value;
+  localStorage.setItem("bio", bio2);
   firebase.auth().onAuthStateChanged(function(user){
     if(user) { //After successful login, user will be redirected to home.html
       userRef.where("id", "==", user.email).limit(1)
   .get()
   .then(function(querySnapshot) {
-
           querySnapshot.forEach(function(doc) {
               userRef.doc(doc.id).update({
                   bio: bio2
               });
 
           });
-
   })
   .catch(function(error) {
       console.log("Error getting documents: ", error);
