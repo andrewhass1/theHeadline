cd9f36da5b082a0e4579dc0742a3ef89fd8c89fe
Robinson24 <andrew.hass@hotmail.com>
2020-02-28 17:40:41 -0500

    new

---
+++ TheHeadline/public/configure copy.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBYcEOx9UUkmT_Xb6oDZ68y2C8W7_Y7hos",
+    authDomain: "theheadline-e09dc.firebaseapp.com",
+    databaseURL: "https://theheadline-e09dc.firebaseio.com",
+    projectId: "theheadline-e09dc",
+    storageBucket: "theheadline-e09dc.appspot.com",
+    messagingSenderId: "776776851089",
+    appId: "1:776776851089:web:5b8f7d3b54e9e44ce92930",
+    measurementId: "G-ZH74YM431H"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
