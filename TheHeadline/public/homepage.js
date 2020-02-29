f80ec8b9009f0e04d493bcb64480bcfd63e9f5ec
Robinson24 <andrew.hass@hotmail.com>
2020-02-29 15:17:02 -0500

    new

--- TheHeadline/public/homepage.js
+++ TheHeadline/public/homepage.js
@@ -7,13 +7,13 @@ function signout(){
     });
   }
 var db = firebase.firestore();
+localStorage.setItem("control", '1null123');
 var postRef = db.collection("posts");
 var stDocRef = postRef.where("status", "==", "NOTPOSTED").orderBy("time", "asc").limit(1);
-
 postRef.where("status", "==", "NOTPOSTED").orderBy("time", "asc").limit(1)
     .get()
     .then(function(querySnapshot) {
-        localStorage.setItem("control", '1null123');
+        
         if (querySnapshot.empty) {
             document.getElementById('aTitle').innerText = "Post something, it will appear right here. Its time for people to hear your voice."
             document.getElementById('aText').innerText = "We beleive that what you say matter and should be heard. If you have something to share post it and it will appear right here immidetly for the world to see! Right now all the post have been shown. If you would like to see more content, tell your friends or write your own. Write something that matters to you. Share your thoughts, your beliefs, or your ideas. Share some news, tell a short story, or write about yourself. \n \n About The Headline. Write about the things you love. It can be anything, a story, an idea, a belief. All that matter is you are passionate about it. Let the world hear your voice. Your post will be on the front page for 5 mins. Everyone will read what you have to say - Finally. All you have to do is pay $1. Nobody likes paying so go into your couch cushion or laundry machine and find a dollar, then post."
@@ -105,3 +105,26 @@ postRef.where("status", "==", "NOTPOSTED").orderBy("time", "asc").limit(1)
 
         }
       });
+
+
+
+      postRef.where("status", "==", "LETTER").orderBy("time", "asc").limit(1)
+    .get()
+    .then(function(querySnapshot) {
+        localStorage.setItem("control", '1null123');
+        if (querySnapshot.empty) {
+            document.getElementById('letterbody1').innerText = "Our articles are written passionate people who want to have their voice heard. Enjoy what people have to say. Discover articles that people write. They know that what they say will be herd, so they put their heart into every post."
+            document.getElementById('letterbody2').innerText = "Come back here to read our editor picks. Our editors will write about their favorite post. They change their pick every other day. Enjoy.";
+
+        } else {
+            querySnapshot.forEach(function(doc) {
+                document.getElementById('letterbody1').innerText = doc.data().title;
+                document.getElementById('letterbody2').innerText = doc.data().content;
+
+            });
+        }
+
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
