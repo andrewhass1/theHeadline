2271e9354489d10f76bb8a20d832a288228467e6
hmacht <32553134+hmacht@users.noreply.github.com>
2020-02-28 20:05:44 -0500

    twitter stuff

--- TheHeadline/public/TwitterPage.js
+++ TheHeadline/public/TwitterPage.js
@@ -1,21 +1,44 @@
+var db = firebase.firestore();
+
 function update(){
-  var userRef = db.collection("users");
   firebase.auth().onAuthStateChanged(function(user){
     if(user) { //After successful login, user will be redirected to home.html
-      userRef.where("id", "==", user.uid).limit(1)
-      .get()
-      .then(function(querySnapshot) {
-              querySnapshot.forEach(function(doc) {
-                  userRef.doc(doc.id).update({
-                      TwitterUN: document.getElementById("inlineFormInputGroupUsername").value
-                  });
-      })
-      .catch(function(error) {
-          console.log("Error getting documents: ", error);
-      });
 
-    } else{
-      location.replace("HomePage.html")
+        var userRef = db.collection("users");
+
+        userRef.where("id", "==", user.uid)
+            .get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    console.log(doc.data().username);
+                    userRef.doc(doc.id).update({
+                        twitterUN: document.getElementById("inlineFormInputGroupUsername").value
+                    });
+
+                    userRef.where("twitterUN", "==", document.getElementById("inlineFormInputGroupUsername").value)
+                    .get()
+                    .then(function(querySnapshot) {
+
+                        location.replace("ReviewPage.html");
+
+
+                    })
+                    .catch(function(error) {
+                        console.log("Error getting documents: ", error);
+                    })
+
+                });
+
+            })
+
+            .catch(function(error) {
+                console.log("Error getting documents: ", error);
+            });
+
+
+
+    } else {
+      location.replace("HomePage.html");
     }
 
   });
